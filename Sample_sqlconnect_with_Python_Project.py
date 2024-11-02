dde24f816e720538b4f90afd14a26f1cfb89aca1
Shafiya-Maas <shafiya.banu@tvs.in>
2024-11-02 15:38:32 +0530

    Update Sample_sqlconnect_with_Python_Project.py

--- Sample_sqlconnect_with_Python_Project.py
+++ Sample_sqlconnect_with_Python_Project.py
@@ -1,10 +1,11 @@
-streamlit
-mysql-connector-python
-pandas
-numpy
-matplotlib
-seaborn
-
+import mysql.connector
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+import streamlit as st 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Connect to MySQL database
 db = mysql.connector.connect(
@@ -21,8 +22,10 @@ cur.execute(query)
 result = cur.fetchall()
 
 # Load data into a DataFrame
-query = """SELECT * FROM city"""
-df = pd.read_sql(query, db)
+df = pd.DataFrame(result, columns=[desc[0] for desc in cur.description])
+
+cur.close()
+db.close()
 
 # Set page configuration
 st.set_page_config(page_title="Sample SQL connect with Python", page_icon=":bar_chart:", layout="wide")
